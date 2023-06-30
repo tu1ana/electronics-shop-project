8e49bc86639e110963a5ca2f8edf6b98a6d58731
Tuyana Chirninova <hikkonki@gmail.com>
2023-06-30 20:33:53 +0300

    add test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+class TestItem:
+    pay_rate = 1.0
+
+    def test_calculate_total_price(self):
+        item1 = Item('Headset', 4000.0, 50)
+        assert item1.calculate_total_price() == 200000.0
+
+    def test_apply_discount(self):
+        item1 = Item('Headset', 4000.0, 50)
+        assert item1.apply_discount() is None
+
