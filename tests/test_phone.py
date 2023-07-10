4cfacd2b0d8731666b2b802fda1de12da2c9f0dc
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-11 01:59:13 +0300

    add tests for class phone

---
+++ tests/test_phone.py
@@ -0,0 +1,19 @@
+from src.phone import Phone
+
+
+def test_init_phone():
+    phone1 = Phone('Samsung Galaxy S23', 100_000, 20, 2)
+    assert phone1.name == 'Samsung Galaxy S23'
+    assert phone1.price == 100_000
+    assert phone1.quantity == 20
+    assert phone1.number_of_sim == 2
+
+
+def test_repr():
+    phone2 = Phone('BlackBerry Bold', 10000, 3, 1)
+    assert repr(phone2) == "Phone('BlackBerry Bold', 10000, 3, 1)"
+
+
+def test_str():
+    phone3 = Phone('Nokia 3310', 3000, 2, 1)
+    assert str(phone3) == 'Nokia 3310'
