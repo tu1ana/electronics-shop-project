9ecf4030a5b0aa5ccbbffa0b9a65452f853790f9
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-15 21:40:55 +0300

    tests to Keyboard added

---
+++ tests/test_keyboard.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.keyboard import Keyboard
+
+
+def test_str():
+    k = Keyboard('Dark Project KD87A', 9600, 5)
+
+    assert str(k) == 'Dark Project KD87A'
+
+
+def test_change_lang():
+    k = Keyboard('Dark Project KD87A', 9600, 5)
+
+    assert k.language == 'EN'
+
+    k.change_lang()
+    assert str(k.language) == 'RU'
+
+    k.change_lang().change_lang()
+    assert str(k.language) == 'RU'
+
+# def test_keyboard_layout():
+#     k = Keyboard('Dark Project KD87A', 9600, 5)
+#     assert k.name == 'Dark Project KD87A'
+#     assert k.price == 9600
+#     assert k.quantity == 5
