84ddee08cf92e16e0e8da949de3d9445a8fbac28
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-18 20:09:22 +0300

    super() func added

--- src/item.py
+++ src/item.py
@@ -21,6 +21,7 @@ class Item:
         self.price = price
         self.quantity = quantity
         self.__class__.all.append(self)
+        super().__init__()
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
