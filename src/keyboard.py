69ba9ca0ecab8c403a20e628695dab4796e64741
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-18 20:10:42 +0300

    corrections

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,32 +3,22 @@ from abc import ABC, abstractmethod
 from src.item import Item
 
 
-class KeyboardLayout(ABC):
+class KeyboardLayout:
 
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
-
-    @abstractmethod
-    def change_lang(self):
-        pass
-
-
-class Keyboard(Item, KeyboardLayout):
-
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
+    def __init__(self):
         self.__language = 'EN'
 
-    def __str__(self):
-        return self.name
+    @property
+    def language(self):
+        return self.__language
 
     def change_lang(self):
-        if self.__language:
+        if self.language:
             self.__language = 'RU'
         else:
             return self.language
         return self
 
-    @property
-    def language(self):
-        return self.__language
+
+class Keyboard(Item, KeyboardLayout):
+    pass
